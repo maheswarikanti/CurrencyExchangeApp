2b61887f3a7f2edd5b51b14d58adcbdfc2a25f13
Maheswari Kanti <kanti.m@northeastern.edu>
2021-11-03 15:57:12 -0700

    Currency Exchange App - Assignment
    Provide two input from currency and to currency and then click on get exchanges rates to display  current prices

--- CurrencyExchange/ViewController.swift
+++ CurrencyExchange/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import SwiftyJSON
+import SwiftSpinner
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    let baseURL = "https://free.currconv.com/api/v7/convert?q="
+    let apiKey = "a414056cac6e2f94dca6"
 
+    @IBOutlet weak var txtBaseCurrency: UITextField!
+    
+    @IBOutlet weak var txtCurrencySymbol: UITextField!
+    
+    @IBOutlet weak var lblCurrencyRate: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getCurrencyConvertedRate(_ sender: Any) {
+        
+        if txtCurrencySymbol.text == "" {
+            return
+        }
+        
+        var curCon = self.txtBaseCurrency.text!.uppercased() + "_" + self.txtCurrencySymbol.text!.uppercased()
+        
+        var url = baseURL + curCon + "&compact=ultra&apiKey=" + apiKey
+        
+        SwiftSpinner.show("Get currency rates")
+        
+        AF.request(url).responseJSON { response in
+            
+            SwiftSpinner.hide()
+            
+            if response.error != nil {
+                print(response.error!)
+                return
+            }
+            
+            let exchange = JSON(response.data!)
+            
+            if exchange.isEmpty == true {
+                print("Currency codes are incorrect")
+                return
+            }
+            
+            var convertedValue = exchange[curCon]
+            
+            self.lblCurrencyRate.text = "Current Rate is: \(convertedValue)"
+        }
+    }
 }
 
